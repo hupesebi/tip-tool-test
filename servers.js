f7e7648041135abb80f8a1edef48576a899efa73
Sebastian Hupe <Sebastian.Hupe@gmail.com>
2021-06-04 08:40:51 +0200

    Wrote test for server.js

--- servers.js
+++ servers.js
@@ -42,3 +42,5 @@ function updateServerTable() {
     serverTbody.append(newTr);
   }
 }
+
+
--- servers.test.js
+++ servers.test.js
@@ -11,7 +11,29 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it ('should not add a server when the string is empty on submitServerInfo()', function(){
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect (Object.keys(allServers).length).toEqual(0);
+  });
+
+  it ('should update the table with the id of servertable on updateServerTable()', function(){
+    submitServerInfo ();
+    updateServerTable ();
+    let table = document.querySelector("#serverTable tbody").querySelectorAll("tr, td");
+    
+    console.log (table)
+    expect (table.length).toEqual(3);
+    expect (table[0].id).toEqual("server1");
+    expect (table[1].innerHTML).toEqual("Alice");
+    expect (table[2].innerHTML).toEqual("$0.00");
+    
+  });
+    
   afterEach(function() {
-    // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
+
   });
 });
